6ba05c3e0d003228de23dc5ef22ca03694142fbe
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-17 14:13:54 -0500

    Did some things

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 console.log("my name is brian i am the bot");
+
+Settings = require("./settings.json")
+
+Responses = require("./Responses.json")
+
+Discord = require('discord.js');
+
+client = new Discord.Client();
+
+client.login(Settings.token);
+
+client.on('message', message => {
+
+    if (client.user.id == message.mentions.users.firstKey()){
+        var messageSplit = message.content.match(/("[^"]*")|[^ ]+/g);
+
+        console.log(messageSplit);
+
+        if(messageSplit[0] == '<@' + client.user.id + '>'){
+            if(messageSplit[1] == "philosophy"){
+                message.reply(Responses.Philosophy);
+            }else if(messageSplit[1] == "about"){
+                message.reply(Responses.About);
+            }else if(messageSplit[1] == "book"){
+                message.reply(Responses.Book);
+            }else{
+                message.reply(Responses.Annoying);
+            }
+        }
+    }
+});
