3e4ce2f98b8fae8c7583234004dea76f7b1b43ea
Alexander Bean Apmann <caboose200@gmail.com>
2017-02-21 12:00:06 -0500

    Started attachment parsing

--- main.js
+++ main.js
@@ -10,6 +10,14 @@ client = new Discord.Client();
 
 client.login(Settings.token);
 
+AttachmentFunctions = {
+    "grade" : attachment => {
+        console.log(attachment.url);
+
+        attachment.message.reply("F");
+    }
+}
+
 client.on('message', message => {
 
     if (message.attachments.size === 0 && client.user.id === message.mentions.users.firstKey()){
@@ -20,16 +28,33 @@ client.on('message', message => {
             var response = Responses[messageSplit[1].toLowerCase()];
 
             if(response === undefined){
-                message.reply(Responses.annoying);
+                message.reply(Responses._annoying);
             } else{
                 message.reply(response);
             }
         }else{
-            message.reply(Responses.annoying);
+            message.reply(Responses._annoying);
         }
     }else if (client.user.id === message.mentions.users.firstKey()){
-        message.reply("Not implemented");
+        var messageSplit = message.content.match(/("[^"]*")|[^ ]+/g);
 
-        //Do things with attachments here
+        var attachment = Array.from(message.attachments.values())[0];
+
+        console.log(attachment, attachment.width);
+
+        if(messageSplit[0] === '<@' + client.user.id + '>' && attachment.width !== undefined && messageSplit.length >= 2){
+            
+            var response = AttachmentFunctions[messageSplit[1].toLowerCase()];
+
+            if(response === undefined){
+                message.reply(Responses._annoyingimage);
+            } else{
+                response(attachment);
+            }
+        }else if (messageSplit.length >= 2){
+            message.reply(Responses._annoyingimage);
+        }else {
+            message.reply(Responses._imagesonly);
+        }
     }
 });
